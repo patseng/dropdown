e855f0e01ceb96a2e8455a34960ed6e1be1e968b
Peter Tseng <patseng@stanford.edu>
2018-02-11 17:11:48 -0800

    Adding react-toolbox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './assets/react-toolbox/theme.css'
+import theme from './assets/react-toolbox/theme.js'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </ThemeProvider>
     );
   }
 }
